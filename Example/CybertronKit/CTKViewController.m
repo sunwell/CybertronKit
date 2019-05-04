b1bfd980be05e1d182b51fb7e411d7440baf4df1
waynesun <waynesun@vipabc.com>
2019-05-04 22:54:15 +0800

    完善自定义按钮样式功能

--- Example/CybertronKit/CTKViewController.m
+++ Example/CybertronKit/CTKViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "CTKViewController.h"
-//#import <>
+#import <CybertronKit/UIButton+CTKExtension.h>
 
 @interface CTKViewController ()
 @property (nonatomic, strong) UIButton *imageLeft;
+@property (nonatomic, strong) UIButton *imageRight;
+@property (nonatomic, strong) UIButton *imageTop;
+@property (nonatomic, strong) UIButton *imageBottom;
 @end
 
 @implementation CTKViewController
@@ -20,13 +23,49 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     [self.view addSubview:self.imageLeft];
-//    [self.imageLeft ctkConfigWithImage:[UIImage imageNamed:@"test"]
-//                                 title:@"测试测试"
-//                             titleFont:[UIFont systemFontOfSize:14]
-//                            titleColor:[UIColor greenColor]
-//                              internal:10
-//                              forState:UIControlStateNormal
-//                              andStyle:CTKButtonStyleImageLeft];
+    [self.imageLeft ctkConfigWithImage:[UIImage imageNamed:@"test"]
+                                 title:@"图片在左"
+                             titleFont:[UIFont systemFontOfSize:14]
+                            titleColor:[UIColor greenColor]
+                              internal:10
+                              forState:UIControlStateNormal
+                              andStyle:CTKButtonStyleImageLeft];
+    self.imageLeft.titleLabel.backgroundColor = [UIColor yellowColor];
+    self.imageLeft.imageView.backgroundColor = [UIColor yellowColor];
+    
+    
+    [self.view addSubview:self.imageRight];
+    [self.imageRight ctkConfigWithImage:[UIImage imageNamed:@"test"]
+                                  title:@"图片在右"
+                              titleFont:[UIFont systemFontOfSize:14]
+                             titleColor:[UIColor greenColor]
+                               internal:10
+                               forState:UIControlStateNormal
+                               andStyle:CTKButtonStyleImageRight];
+    self.imageRight.titleLabel.backgroundColor = [UIColor yellowColor];
+    self.imageRight.imageView.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:self.imageTop];
+    [self.imageTop ctkConfigWithImage:[UIImage imageNamed:@"test"]
+                                title:@"图片在上面"
+                            titleFont:[UIFont systemFontOfSize:14]
+                           titleColor:[UIColor greenColor]
+                             internal:10
+                             forState:UIControlStateNormal
+                             andStyle:CTKButtonStyleImageTop];
+    self.imageTop.titleLabel.backgroundColor = [UIColor yellowColor];
+    self.imageTop.imageView.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:self.imageBottom];
+    [self.imageBottom ctkConfigWithImage:[UIImage imageNamed:@"test"]
+                                   title:@"图片在下面"
+                               titleFont:[UIFont systemFontOfSize:14]
+                              titleColor:[UIColor greenColor]
+                                internal:10
+                                forState:UIControlStateNormal
+                                andStyle:CTKButtonStyleImageBottom];
+    self.imageBottom.titleLabel.backgroundColor = [UIColor yellowColor];
+    self.imageBottom.imageView.backgroundColor = [UIColor yellowColor];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,9 +76,34 @@
 #pragma mark - getter
 - (UIButton *)imageLeft {
     if (!_imageLeft) {
-        _imageLeft = [[UIButton alloc] initWithFrame:CGRectMake(10, 100, 100, 44)];
+        _imageLeft = [[UIButton alloc] initWithFrame:CGRectMake(10, 100, 200, 66)];
+        _imageLeft.backgroundColor = [UIColor grayColor];
     }
     return _imageLeft;
+}
+
+- (UIButton *)imageRight {
+    if (!_imageRight) {
+        _imageRight = [[UIButton alloc] initWithFrame:CGRectMake(10, 200, 200, 66)];
+        _imageRight.backgroundColor = [UIColor grayColor];
+    }
+    return _imageRight;
+}
+
+- (UIButton *)imageTop {
+    if (!_imageTop) {
+        _imageTop = [[UIButton alloc] initWithFrame:CGRectMake(10, 300, 200, 66)];
+        _imageTop.backgroundColor = [UIColor grayColor];
+    }
+    return _imageTop;
+}
+
+- (UIButton *)imageBottom {
+    if (!_imageBottom) {
+        _imageBottom = [[UIButton alloc] initWithFrame:CGRectMake(10, 400, 200, 66)];
+        _imageBottom.backgroundColor = [UIColor grayColor];
+    }
+    return _imageBottom;
 }
 
 @end
